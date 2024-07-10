648c8755736158c148c5fd8f12a58a69b4082713
Ricardo Perez <rxperez12@gmail.com>
2024-07-10 16:21:01 -0700

    Finish numGreater function

--- trees/num-greater/numGreater.ts
+++ trees/num-greater/numGreater.ts
@@ -1,11 +1,36 @@
 import { TreeNodeNum } from "../common/tree";
+import { Stack } from "../common/stack";
+import { isWhileStatement } from "typescript";
 
 /** numGreater(lowerBound): starting from the invoking node and moving
  * through its children, return a count of the number of nodes whose value
  * is greater than lowerBound. */
 
 function numGreater(node: TreeNodeNum, lowerBound: number): number {
-  return 42;
+  const nodesToVisit = new Stack([node])
+
+  let answer = 0;
+  while(!nodesToVisit.isEmpty()){
+    const current = nodesToVisit.pop()
+
+    if(current.val > lowerBound){
+      answer +=  1;
+    }
+
+    for(const child of current.children){
+      nodesToVisit.push(child)
+    }
+  }
+  return answer;
 }
 
 export { numGreater };
+
+/**
+//         1
+//         |
+//         2
+
+ *
+ *
+ */
