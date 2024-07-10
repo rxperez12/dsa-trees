25f761bef54e5c53bec67f97e88baddf14707de7
Ricardo Perez <rxperez12@gmail.com>
2024-07-10 12:03:47 -0700

    Inital files

---
+++ trees/pre-post-order/prePostOrder.ts
@@ -0,0 +1,18 @@
+import { TreeNodeNum } from "../common/tree";
+
+/** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
+ * Returns an array of values of visited nodes. */
+
+function preOrder(node: TreeNodeNum | null): number[] {
+  return [42];
+}
+
+
+/** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
+ * Returns an array of values of visited nodes. */
+
+function postOrder(node: TreeNodeNum | null): number[] {
+  return [42];
+}
+
+export { preOrder, postOrder };
